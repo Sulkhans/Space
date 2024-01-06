59336cd6ad53d82223fd30b47a0a413c0ee0146b
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-01-06 21:23:08 +0400

    Handle loading

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
+import { auth } from "../config/firebase";
 import Sidebar from "./Sidebar";
+import Loading from "./Loading";
 
 type LayoutProps = {
   children: JSX.Element;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  return (
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const checkAuth = auth.onAuthStateChanged(() => setLoading(false));
+    return () => checkAuth();
+  }, []);
+  return loading ? (
+    <Loading />
+  ) : (
     <div className="flex">
       <Sidebar />
       <div>{children}</div>
