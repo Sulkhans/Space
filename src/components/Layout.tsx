972851b72370e1acbe644c413d9e468bb73126a5
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-01-07 16:30:05 +0400

    Responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
-import { auth } from "../config/firebase";
 import Sidebar from "./Sidebar";
 import Loading from "./Loading";
+import useAuthLoad from "../hooks/useAuthLoad";
+import { Navigate } from "react-router-dom";
 
 type LayoutProps = {
   children: JSX.Element;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  const [loading, setLoading] = useState<boolean>(true);
+  const { loading, user } = useAuthLoad();
+  const [isHidden, setIsHidden] = useState<boolean>(true);
+  const [width, setWidth] = useState<number>(window.innerWidth);
   useEffect(() => {
-    const checkAuth = auth.onAuthStateChanged(() => setLoading(false));
-    return () => checkAuth();
+    const screenResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", screenResize);
+    setIsHidden(width <= 768);
+    return () => window.removeEventListener("resize", screenResize);
   }, []);
   return loading ? (
     <Loading />
-  ) : (
+  ) : user ? (
     <div className="flex">
-      <Sidebar />
-      <div>{children}</div>
+      <Sidebar isHidden={isHidden} width={width} />
+      <div
+        className={`p-4 transition-all duration-1000
+        ${width >= 768 && !isHidden && "ml-48"} `}
+      >
+        <button className="absolute" onClick={() => setIsHidden(!isHidden)}>
+          Navbar
+        </button>
+        {children}
+      </div>
     </div>
+  ) : (
+    <Navigate to={`/Space/auth`} />
   );
 };
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,17 @@
 import { auth } from "../config/firebase";
 
-const Sidebar = () => {
+type SidebarProps = {
+  isHidden: boolean;
+  width: number;
+};
+
+const Sidebar = ({ isHidden, width }: SidebarProps) => {
   return (
-    <div className="fixed flex flex-col items-center p-4 w-48 h-screen bg-[#050505] shadow-lg shadow-black text-white">
+    <div
+      className={`fixed z-50 w-full flex flex-col items-center p-4 h-screen bg-[#050505] shadow-lg shadow-black text-white duration-1000 transition-all sm:w-48 
+      ${isHidden && width <= 768 && "-translate-x-[105%]"}
+      ${isHidden && width >= 768 && "-translate-x-52"}`}
+    >
       <h1 className="text-3xl font-semibold select-none">Space</h1>
       <p className="text-lg font-semibold">{auth.currentUser?.displayName}</p>
     </div>
