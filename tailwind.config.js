59f029281cf2030e764979adb5fc7f53a838115c
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-01-07 21:10:56 +0400

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
       fontFamily: {
         sans: ["Quicksand", "sans-serif"],
       },
+      boxShadow: {
+        def: "0px 0px 1rem rgba(0, 0, 0, 0.5)",
+      },
+      colors: {
+        dark: "#050505",
+      },
     },
   },
   plugins: [],
