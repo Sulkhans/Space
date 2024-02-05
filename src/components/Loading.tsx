c058bceac7cca759b7ac05ee26c375c14d698088
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-02-05 21:43:28 +0400

    Animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,7 @@
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="w-10 h-10 border-4 border-neutral-900 border-b-transparent border-t-transparent rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-sp" />
+  );
 };
 
 export default Loading;
