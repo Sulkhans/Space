4c5f2361e3f82b992e61301b5394769d2c99783c
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-02-06 16:52:24 +0400

    Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,8 @@
 const Loading = () => {
   return (
-    <div className="w-10 h-10 border-4 border-neutral-900 border-b-transparent border-t-transparent rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-sp" />
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="w-10 h-10 border-4 border-neutral-900 border-b-transparent border-t-transparent rounded-full animate-sp" />
+    </div>
   );
 };
 
