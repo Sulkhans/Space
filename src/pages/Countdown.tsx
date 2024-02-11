79f0d9e0c49cbe7001ade66baf72dfb9e22db778
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-02-12 00:50:32 +0400

    Countdown

--- src/pages/Countdown.tsx
+++ src/pages/Countdown.tsx
@@ -1,22 +1,32 @@
 import { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db } from "../config/firebase";
 import useAuthLoad from "../hooks/useAuthLoad";
 import Loading from "../components/Loading";
 import { Button } from "../components/Button";
 //@ts-ignore
 import Delete from "../assets/trash.svg?react";
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "../config/firebase";
+//@ts-ignore
+import Back from "../assets/back.svg?react";
 
 type countdownType = {
   id: string;
   title: string;
-  date: Date;
+  date: number;
   timeLeft: number;
 };
 
 export const Countdown = () => {
   const [countdowns, setCountdowns] = useState<Array<countdownType>>([]);
-  const [current, setCurrent] = useState<countdownType>();
+  const [current, setCurrent] = useState<number>();
   const [editor, setEditor] = useState<boolean>(false);
   const [inputTitle, setInputTitle] = useState<string>("");
   const [inputDate, setInputDate] = useState<string>("");
@@ -24,24 +34,61 @@ export const Countdown = () => {
   const { user, loading } = useAuthLoad();
 
   useEffect(() => {
+    const fetch = async () => {
+      const countdownRef = collection(db, "users", user!.uid, "countdown");
+      const q = query(countdownRef, orderBy("date"));
+      const res = await getDocs(q);
+      const fetched = res.docs.map((doc) => ({
+        id: doc.id,
+        timeLeft: 0,
+        ...doc.data(),
+      }));
+      setCountdowns(fetched as countdownType[]);
+      setFetching(false);
+    };
+    if (!loading) fetch();
+  }, [user, fetching]);
+
+  useEffect(() => {
     const interval = setInterval(() => {
       setCountdowns((prevCountdowns) => {
         const now = new Date().getTime() / 1000;
         const updatedCountdowns = prevCountdowns.map((item) => {
-          const until = item.date.getTime() / 1000;
+          const until = item.date / 1000;
           const TimeLeft = until - now;
           return {
             ...item,
-            timeLeft: TimeLeft,
+            timeLeft: TimeLeft > 0 ? TimeLeft : 0,
           };
         });
         return updatedCountdowns;
       });
     }, 1000);
-
     return () => clearInterval(interval);
   }, [countdowns]);
 
+  const handleNew = async () => {
+    if (inputTitle.replace(/ /g, "") != "" && inputDate != "") {
+      const countdownRef = collection(db, "users", user!.uid, "countdown");
+      await addDoc(countdownRef, {
+        title: inputTitle,
+        date: new Date(inputDate).getTime(),
+      });
+      setFetching(true);
+      closeEditor();
+    }
+  };
+  const handleDelete = async () => {
+    const countdownRef = collection(db, "users", user!.uid, "countdown");
+    await deleteDoc(doc(countdownRef, countdowns[current!]?.id));
+    setFetching(true);
+    setCurrent(undefined);
+  };
+  const closeEditor = () => {
+    setInputTitle("");
+    setInputDate("");
+    setEditor(false);
+  };
   const format = (time: number) => {
     const day = Math.floor(time / 86400);
     const hr = Math.floor((time % 86400) / 3600);
@@ -49,8 +96,8 @@ export const Countdown = () => {
     const sec = Math.floor(time % 60);
     return [day, hr, min, sec];
   };
-  const formatDate = (date: Date) =>
-    date.toLocaleString("de-DE", {
+  const formatDate = (date: number) =>
+    new Date(date).toLocaleString("de-DE", {
       day: "numeric",
       month: "2-digit",
       year: "numeric",
@@ -58,28 +105,42 @@ export const Countdown = () => {
       minute: "numeric",
       hour12: true,
     });
-  const closeEditor = () => {
-    setInputTitle("");
-    setInputDate("");
-    setEditor(false);
-  };
-  const handleNew = async () => {
-    if (inputTitle.replace(/ /g, "") != "" && inputDate != "") {
-      const countdownRef = collection(db, "users", user!.uid, "countdown");
-      await addDoc(countdownRef, {
-        title: inputTitle,
-        date: inputDate,
-      });
-      setFetching(true);
-      closeEditor();
-    }
-  };
 
   return fetching && countdowns.length === 0 ? (
     <Loading />
+  ) : current != undefined ? (
+    <div>
+      <div className="h-10 flex items-center justify-between">
+        <Back
+          onClick={() => {
+            setCurrent(undefined);
+          }}
+          className="w-10 h-10 p-0.5 shadow-md cursor-pointer border-2 border-neutral-900 rounded-md"
+        />
+        <Delete
+          onClick={handleDelete}
+          className="w-10 h-10 p-2 shadow-md cursor-pointer border-2 border-neutral-900 rounded-md hover:fill-red-600 transition-all"
+        />
+      </div>
+      <div className="flex flex-col items-center gap-4 md:gap-10 xl:gap-16 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center font-bold select-none">
+        <p className="w-screen text-2xl md:text-4xl xl:text-6xl">
+          {countdowns[current].title}
+        </p>
+        <div className="px-3 grid grid-cols-4 grid-rows-2 place-items-center text-sm md:text-2xl xl:text-4xl gap-x-6 md:gap-x-16 md:gap-y-4">
+          {format(countdowns[current].timeLeft).map((val, i) => (
+            <p key={i} className="leading-7 text-3xl md:text-6xl md:leading-7">
+              {val}
+            </p>
+          ))}
+          <p>DAYS</p>
+          <p>HOURS</p>
+          <p>MINS</p>
+          <p>SECS</p>
+        </div>
+      </div>
+    </div>
   ) : (
     <div className="flex flex-col gap-3">
-      <p>{new Date("2024-02-22T22:24").getTime()}</p>
       <Button
         onClick={() => setEditor(true)}
         text="Create a new countdown"
@@ -113,6 +174,7 @@ export const Countdown = () => {
         {countdowns.map((item, i) => (
           <div
             key={i}
+            onClick={() => setCurrent(i)}
             className="p-3 flex flex-col items-center gap-3 sm:gap-5 md:flex-grow border-2 border-neutral-900 font-bold shadow-md rounded-md select-none cursor-pointer transition-all"
           >
             <p className="text-xl sm:text-2xl">{item.title}</p>
