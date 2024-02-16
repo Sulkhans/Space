2d4e84bb338b289e68bb5f8dc513c4086afe20b7
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-02-16 16:39:55 +0400

    Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,10 @@
 import Calendar from "../components/Calendar";
+import Quote from "../components/Quote";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col gap-3 md:flex-row">
+    <div className="flex flex-col lg:grid lg:grid-cols-[1fr_20rem] gap-3">
+      <Quote />
       <Calendar />
     </div>
   );
